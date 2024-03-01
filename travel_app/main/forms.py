bfbca33ac5c078980360e698bbb9061ae0deaca5
mathyasp <mathyastp@gmail.com>
2024-03-01 10:40:28 -0500

    added forms

---
+++ travel_app/main/forms.py
@@ -0,0 +1,71 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, FloatField, SelectField, SubmitField, ValidationError
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms.validators import DataRequired, Length, NumberRange
+from travel_app.models import Country, Trip, User
+
+class CountryForm(FlaskForm):
+    name = StringField("Country Name", 
+      validators=[
+        DataRequired(), 
+        Length(min=2, max=50)
+      ])
+    climate = StringField("Climate", 
+      validators=[
+        DataRequired(), 
+        Length(min=2, max=50)
+      ])
+    language = StringField("Language", 
+      validators=[
+        DataRequired(), 
+        Length(min=2, max=50)
+      ])
+    img_url = StringField("Image URL", 
+      validators=[
+        DataRequired()
+      ])
+    submit = SubmitField("Submit")
+
+class TripForm(FlaskForm):
+    trip_type = SelectField("Trip Type", 
+      choices=TripType.choices(), 
+      validators=[
+        DataRequired()
+      ])
+    past_or_future = SelectField("Past or Future", 
+      choices=PastOrFuture.choices(), 
+      validators=[
+        DataRequired()
+      ])
+    country = QuerySelectField("Country", 
+      query_factory=lambda: Country.query, 
+      allow_blank=False, 
+      get_label="name")
+    year = DateField("Year", 
+      validators=[
+        DataRequired()
+      ])
+    trip_length = FloatField("Trip Length", 
+      validators=[
+        DataRequired(), 
+        NumberRange(min=1, max=365)
+      ])
+    highlight = StringField("Highlight", 
+      validators=[
+        DataRequired(), 
+        validate_time_format
+      ])
+    submit = SubmitField("Submit")
+
+    @staticmethod
+    def validate_time_format(form, field):
+      """Validate time format in the form 'Xd, Xw, Xm' (X days, X weeks, X months)"""
+      time_units = field.data.split(',')
+      for unit in time_units:
+        unit = unit.strip()
+        if not (unit.endswith('d') or unit.endswith('w') or unit.endswith('m')):
+          raise ValidationError('Time must be in the format "Xd, Xw, Xm"')
+        try:
+          int(unit[:-1])
+        except ValueError:
+          raise ValidationError('Time must be in the format "Xd, Xw, Xm"')
--- travel_app/models.py
+++ travel_app/models.py
@@ -4,6 +4,26 @@ from flask_login import UserMixin
 from grocery_app.extensions import db
 from grocery_app.utils import FormEnum
 
+class ClimateType(FormEnum):
+  """Different climate types"""
+  tropical = "Tropical"
+  arid = "Arid"
+  temperate = "Temperate"
+  continental = "Continental"
+  polar = "Polar"
+
+class TripType(FormEnum):
+  """Different trip types"""
+  leisure = "Leisure"
+  business = "Business"
+  family = "Family"
+  adventure = "Adventure"
+
+class PastOrFuture(FormEnum):
+  """Past or future trip"""
+  past = "Past"
+  future = "Future"
+
 class Country(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -12,25 +32,25 @@ class Country(db.Model):
     img_url = db.Column(URLType)
 
     def __str__(self):
-        return f'<Country: {self.name}'
+        return f"<Country: {self.name}"
     
     def __repr__(self):
-        return f'<Country: {self.name}>'
+        return f"<Country: {self.name}>"
 
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     trip_type = db.Column(db.String(50), nullable=False)
     past_or_future = db.Column(db.String(50), nullable=False)
-    country = db.relationship('Country', back_populates='country')
+    country = db.relationship("Country", back_populates="country")
     year = db.Column(db.Integer, nullable=False)
     trip_length = db.Column(db.Integer, nullable=False)
     highlight = db.Column(db.String(200), nullable=False)
 
     def __str__(self):
-        return f'<TripType: {self.name}'
+        return f"<TripType: {self.name}"
     
     def __repr__(self):
-        return f'<TripType: {self.name}>'
+        return f"<TripType: {self.name}>"
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -38,16 +58,16 @@ class User(UserMixin, db.Model):
   password = db.Column(db.String(200), nullable=False)
   name = db.Column(db.String(80), nullable=False)
   profile_pic_url = db.Column(URLType)
-  trips = db.relationship('Trip', secondary=user_trips_table, back_populates='trips')
+  trips = db.relationship("Trip", secondary=user_trips_table, back_populates="trips")
 
   def __str__(self):
-    return f'<User: {self.username}>'
+    return f"<User: {self.username}>"
 
   def __repr__(self):
-    return f'<User: {self.username}>'
+    return f"<User: {self.username}>"
 
-user_trips_table = db.Table('user_trips',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('trip_id', db.Integer, db.ForeignKey('trip.id')),
-    PrimaryKeyConstraint('user_id', 'trip_id')
+user_trips_table = db.Table("user_trips",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("trip_id", db.Integer, db.ForeignKey("trip.id")),
+    PrimaryKeyConstraint("user_id", "trip_id")
 )
